138c5e1ff0ec58895c239f0b593fd85142cb5673
lichee <luleier@gmail.com>
2014-04-29 10:44:07 +0800

    can generate a sin signal now

--- RTL_PY/play_sin.py
+++ RTL_PY/play_sin.py
@@ -1,54 +1,74 @@
 # -*- coding: utf-8 -*-
 import pyaudio
 import wave
+import time
+from struct import *
 
 from scipy import arange
 from numpy import pi
-from pylab import cos
+from pylab import cos,sin
 from matplotlib import pyplot
 
-freq = 500.0
-sam_rate=6000.0
-N     = 6000
-T     = arange(0.0,float(N),1.0)
-xArray = cos(2.0*pi*freq*T/sam_rate) * 256
-xArray.astype(int)
-
-#chunk = 1024
-
-#wf = wave.open(r"c:\WINDOWS\Media\ding.wav", 'rb')
+freq = 100
+sam_rate=44100
+N     = 44100
+T     = range(0,sam_rate/freq,1)
+Amp   = 32000
+Sample_Len = 5
 
 p = pyaudio.PyAudio()
 fig1 = pyplot.figure()
 
-# open stream
-#channel = wf.getnchannels()
 channel = 1
-#rates = wf.getframerate()
 rates = int(sam_rate)
-#format1 = p.get_format_from_width(wf.getsampwidth())
-format1 = 8 #this means 16bit int?
+format1 = pyaudio.paInt16
+
+phase = pi/2
+phase = 0
+counter = 2
+xArray = ""
+datalst = []
+
+for j in range(Sample_Len):
+    for i in T:
+        tempf=sin(2.0*pi*float(freq)*float(i)/float(sam_rate) + float(phase)) * float(Amp)
+        datalst.append(int(tempf))
+        #print int(tempf)
+        xArray = xArray + pack('h',int(tempf))
+#print unpack('hhh',xArray[0:6])
+print "calc finished"
+print len(xArray)
+#pyplot.plot(datalst)
+#pyplot.show()
+
+def callback(in_data, frame_count, time_info, status):
+    data=xArray[callback.offset:frame_count*2+callback.offset]
+    callback.offset = (frame_count*2+callback.offset)%(sam_rate/freq*2)
+    return (data, pyaudio.paContinue)
+
+callback.offset = 0
 
 stream = p.open(format = format1,
                 channels=channel,
                 rate=rates,
-                output = True)
+                output = True,
+                stream_callback=callback)
 
-# write stream to play
-phase = pi/2
-counter = 200
-xArray = cos(2.0*pi*freq*T/sam_rate + phase) * 2048
-data = xArray.astype(int)
-while True:
-    #data = wf.readframes(chunk)
-    stream.write(data)
-    xArray = cos(2.0*pi*freq*T/sam_rate + phase) * 1000
-    data = xArray.astype(int)
-    counter = counter - 1
-    if counter == 0: break
-    #pyplot.plot(data)
-    #pyplot.show()
+stream.start_stream()
 
+while stream.is_active():
+    time.sleep(0.01)
 
+#while True:
+    ##data = wf.readframes(chunk)
+    #stream.write(data)
+    ##xArray = cos(2.0*pi*freq*T/sam_rate + phase) * Amp
+    ##data = xArray.astype(int)
+    #counter = counter - 1
+    #if counter == 0: break
+    ##pyplot.plot(data)
+    ##pyplot.show()
+
+stream.stop_stream()
 stream.close()
 p.terminate()
